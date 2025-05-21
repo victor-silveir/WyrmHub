e373734cdd0b140b5ac6cc10704ab745ecc0a36d
Victor Silveira <victor.freitassilveira-ext@ab-inbev.com>
2025-05-21 19:35:56 -0300

    feat: configure axios and react-query

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,20 @@ import { GlobalStyles } from "./styles/GlobalStyles";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/Routes";
 import { AuthProvider } from "./contexts/AuthContext";
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "./api/react-query";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <AuthProvider>
-          <GlobalStyles />
-          <AppRoutes />
-        </AuthProvider>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <AuthProvider>
+            <GlobalStyles />
+            <AppRoutes />
+          </AuthProvider>
+        </BrowserRouter>
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
---
+++ src/api/axios.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default api;
