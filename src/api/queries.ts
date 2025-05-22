7fa3d53f1083086e833a1892a9cbed7a464bdeaf
Victor Silveira <victor.freitassilveira-ext@ab-inbev.com>
2025-05-21 21:57:24 -0300

    feat: create queries to fetch a dragon

--- src/api/queries.ts
+++ src/api/queries.ts
@@ -5,3 +5,8 @@ export const fetchDragons = async (): Promise<Dragon[]> => {
   const response = await api.get("/");
   return response.data;
 };
+
+export const fetchDragonById = async (id: string): Promise<Dragon> => {
+  const { data } = await api.get(`/${id}`);
+  return data;
+};
--- src/hooks/useFetchDragons.ts
+++ src/hooks/useFetchDragons.ts
@@ -1,10 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
-import { fetchDragons } from "../api/queries";
+import { fetchDragonById, fetchDragons } from "../api/queries";
 import Dragon from "../models/dragon";
 
 export const useFetchDragons = () => {
   return useQuery<Dragon[]>({
     queryKey: ["dragons"],
     queryFn: fetchDragons,
+  });
+};
+
+export const useFetchDragonById = (id: string | undefined) => {
+  return useQuery<Dragon>({
+    queryKey: ["dragon", id],
+    queryFn: () => fetchDragonById(id as string),
+    enabled: !!id,
   });
 };
