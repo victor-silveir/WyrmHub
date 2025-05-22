20956aba59f18f336ab2e3bbd2ff28d5373f510e
Victor Silveira <victor.freitassilveira-ext@ab-inbev.com>
2025-05-21 21:10:03 -0300

    feat: create dragons list page

---
+++ src/pages/Dashboard/DragonsPage.style.ts
@@ -0,0 +1,9 @@
+// src/pages/Dragons/DragonsPage.styles.ts
+import styled from "styled-components";
+
+export const PageTitle = styled.h1`
+  font-size: 2rem;
+  margin-bottom: 2rem;
+  text-align: left;
+  color: ${({ theme }) => theme.colors.secondary};
+`;
---
+++ src/pages/Dashboard/DragonsPage.tsx
@@ -0,0 +1,35 @@
+import React, { JSX } from "react";
+import InfiniteScrollList from "../../components/InfinityScroll/InfinityScroll";
+import Card from "../../components/Card/Card";
+import { useFetchDragons } from "../../hooks/useFetchDragons";
+import * as S from "./DragonsPage.style";
+import Alert from "../../components/Alert/Alert";
+import Spinner from "../../components/Spinner/Spinner";
+
+const DragonsPage: React.FC = (): JSX.Element => {
+  const { data: dragons, isLoading, error } = useFetchDragons();
+
+  return (
+    <>
+      <S.PageTitle>Dragons</S.PageTitle>
+      {<Spinner isLoading={isLoading} />}
+      {error && <Alert message="Error loading Dragons data, please reload the page" type="error" />}
+      {dragons && (
+        <InfiniteScrollList
+          data={dragons}
+          pageSize={8}
+          renderItem={(dragon) => (
+            <Card
+              key={dragon.id}
+              title={dragon.name}
+              subtitle={dragon.type}
+              image={dragon.imageUrl}
+            />
+          )}
+        />
+      )}
+    </>
+  );
+};
+
+export default DragonsPage;
